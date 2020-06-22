668fb474bf383e8da0ac60af06ede314a01d898c
Emin Fedar <eminfedar@gmail.com>
2020-06-22 14:18:04 +0300

    updated: application_id

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from MainWindow import MainWindow
 
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="tr.org.pardus.javakur", **kwargs)
+        super().__init__(*args, application_id="tr.org.pardus.java-installer", **kwargs)
         self.window = None
     
     def do_activate(self):
